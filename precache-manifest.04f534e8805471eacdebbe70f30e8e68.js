b4f115ad14233829b4a116e6e051cf5f86333a1a
Trevor Li <lili-rovert@hotmail.com>
2018-12-10 00:02:01 -0700

    Updates

---
+++ precache-manifest.04f534e8805471eacdebbe70f30e8e68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35ba6b3d1566a8943c6b",
+    "url": "/reactClickGame/static/js/runtime~main.35ba6b3d.js"
+  },
+  {
+    "revision": "a738eecca6342c6f4e02",
+    "url": "/reactClickGame/static/js/main.a738eecc.chunk.js"
+  },
+  {
+    "revision": "f4bb04f8a4ae323e35e0",
+    "url": "/reactClickGame/static/js/1.f4bb04f8.chunk.js"
+  },
+  {
+    "revision": "a738eecca6342c6f4e02",
+    "url": "/reactClickGame/static/css/main.5f9a3a74.chunk.css"
+  },
+  {
+    "revision": "5705b8f5d0574577eda0a7d62be769cd",
+    "url": "/reactClickGame/index.html"
+  }
+];
